313428fc4a99283aeccbf39612f51212571f9654
Alexandr Kabanau <Azraill1989@gmail.com>
2022-12-16 21:00:34 +0300

    feat: header - done

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ const baseConfig = {
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
-    new FaviconsWebpackPlugin("./src/img/favicon-32x32.png"),
+    new FaviconsWebpackPlugin("./src/img/icons8-online-store-16.png"),
     new EslingPlugin({ extensions: "ts" }),
     new CleanWebpackPlugin(),
     // eslint-disable-next-line no-undef
