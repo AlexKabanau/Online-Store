b849c55f3303bab1e484dc63030905069e02788c
Alexandr Kabanau <Azraill1989@gmail.com>
2022-12-17 20:44:48 +0300

    add: add dataBase of products

---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+interface ProductItemData {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: Array<string>;
+}
+type CallBackType<T> = (data: T) => void;
+
+export { ProductItemData, CallBackType };
