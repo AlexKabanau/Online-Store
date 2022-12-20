3dc3ee3380aeda05d7cb1c581c49caa710af289f
Alexandr Kabanau <Azraill1989@gmail.com>
2022-12-20 20:39:48 +0300

    add: implement main catalog

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,17 +1,22 @@
 // import productsData from "./components/data";
 import Filter from "./main/Filter";
+import Catalog from "./main/catalog";
+import productsData from "./data";
 class App {
-  container: HTMLElement | null = document.querySelector("main");
+  container: HTMLElement = document.body;
 
   filter: Filter;
+  catalog: Catalog;
 
   constructor() {
     this.filter = new Filter("main", "main", "main");
+    this.catalog = new Catalog("main", "main", "main", productsData);
   }
 
   run() {
     if (this.container) {
       this.container.append(this.filter.render());
+      this.container.append(this.catalog.render(productsData));
     }
   }
 }
