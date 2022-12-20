7566ecea58db9e7d0437d6562d162c3d68434ea7
Alexandr Kabanau <Azraill1989@gmail.com>
2022-12-20 19:57:08 +0300

    add: add catalog-list

--- src/components/Componets.ts
+++ src/components/Componets.ts
@@ -1,13 +1,19 @@
+import { ProductItemData } from "../types";
 class Componets {
   container: HTMLElement;
 
-  constructor(tagName: string, className: string, id: string) {
+  constructor(
+    tagName: string,
+    className: string,
+    id: string,
+    data?: Array<ProductItemData>
+  ) {
     this.container = document.createElement(tagName);
     this.container.className = className;
     this.container.id = id;
   }
 
-  render() {
+  render(data?: Array<ProductItemData>) {
     return this.container;
   }
 }
