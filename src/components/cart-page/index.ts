0402800aae020dce18f8fad64edf79541039385b
Alexandr Kabanau <Azraill1989@gmail.com>
2023-01-03 23:01:28 +0300

    add: add show/hide popupand some check data

--- src/components/cart-page/index.ts
+++ src/components/cart-page/index.ts
@@ -7,21 +7,103 @@ class CartPage extends Page {
   };
 
   //modal popUp
-  // modal: Modal;
+  modal: Modal;
 
   constructor(id: string) {
     super(id);
 
     //modal popUp
-    // this.modal = new Modal("section", "", "modal");
+    this.modal = new Modal("section", "", "modal");
+  }
+
+  afterRender() {
+    function modalPopUp() {
+      const cardInfoButton: HTMLElement | null = document.querySelector(
+        ".checkButton"
+      );
+      const modalPayment: HTMLElement | null = document.querySelector(
+        ".modal-payment"
+      );
+      const modalClose: HTMLElement | null = document.querySelector(
+        ".modal-close"
+      );
+      cardInfoButton?.addEventListener("click", () => {
+        // event.preventDefault();
+        modalPayment?.classList.add("modal-show");
+      });
+      modalClose?.addEventListener("click", () => {
+        modalPayment?.classList.remove("modal-show");
+        modalPayment?.classList.remove("modal-error");
+      });
+      window.addEventListener("keydown", (event) => {
+        if (event.keyCode === 27) {
+          modalPayment?.classList.remove("modal-show");
+          modalPayment?.classList.remove("modal-error");
+        }
+      });
+    }
+    function modalCheck() {
+      const modalPayment: HTMLElement | null = document.querySelector(
+        ".modal-payment"
+      );
+      const paymentForm: HTMLFormElement | null = document.querySelector(
+        ".payment-form"
+      );
+      const paymentName = document.getElementById(
+        "payment-name-input"
+      ) as HTMLInputElement;
+      const paymentEmail: HTMLInputElement | null = document.getElementById(
+        "payment-email-input"
+      ) as HTMLInputElement;
+      const paymentAdress = document.getElementById(
+        "payment-address-input"
+      ) as HTMLInputElement;
+      const paymentPhone = document.getElementById(
+        "payment-phone-input"
+      ) as HTMLInputElement;
+      const paymentCardNumber = document.getElementById(
+        "payment-card-number-input"
+      ) as HTMLInputElement;
+      const paymentCardValid = document.getElementById(
+        "payment-card-valid-input"
+      ) as HTMLInputElement;
+      const paymentCardCVV = document.getElementById(
+        "payment-card-CVV-input"
+      ) as HTMLInputElement;
+      paymentForm?.addEventListener("submit", () => {
+        if (
+          !paymentName?.value ||
+          !paymentEmail?.value ||
+          !paymentAdress?.value ||
+          !paymentPhone?.value ||
+          !paymentCardNumber?.value ||
+          !paymentCardValid?.value ||
+          !paymentCardCVV?.value
+        ) {
+          console.log("submit");
+          modalPayment?.classList.add("modal-error");
+        }
+      });
+    }
+
+    modalPopUp();
+    modalCheck();
   }
 
   render() {
     const title = this.createHeaderTitle(CartPage.TextObject.MainTitle);
     this.container.append(title);
-
+    // кнопка-заглушка
+    const cardInfoButton: HTMLAnchorElement = document.createElement("a");
+    cardInfoButton.className = "checkButton";
+    // cardInfoButton.href = "#";
+    cardInfoButton.innerText = "click for show";
+    this.container.append(cardInfoButton);
     //modal popUp
-    // this.container.append(this.modal.render());
+    this.container.append(this.modal.render());
+    setTimeout(() => {
+      this.afterRender();
+    }, 0);
     return this.container;
   }
 }
